da89667554f2807da94ef4b84bda7f9f44004412
mio <mio4kon@sina.com>
2014-09-10 11:46:42 +0800

    add qbDemo
    add qbDemo

---
+++ unread/糗事百科项目源码/qiushibaike/jikekaifa.com--qiushibaike/src/com/anjoyo/anjoyoqiushibaike/XiaozhitiaoActivity.java
@@ -0,0 +1,17 @@
+package com.anjoyo.anjoyoqiushibaike;
+
+import android.app.Activity;
+import android.os.Bundle;
+//www.javaapk.com
+public class XiaozhitiaoActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_xiaozhitiao);
+		initView();
+	}
+	private void initView() {
+		
+	}
+}
